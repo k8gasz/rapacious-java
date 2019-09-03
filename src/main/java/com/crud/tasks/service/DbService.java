a09f2c3df6f903254c60e62dfe095712dde7a437
k8gasz <gaszewska@pm.me>
2019-09-03 23:27:20 +0200

    CREATE, READ, UPDATE and DELETE methods implementation. DELETE is still in progress.

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -14,5 +15,18 @@ public class DbService {
 
     public List<Task> getAllTasks() {
         return repository.findAll();
+    }
+
+    /*public void deleteTask(final Long id) {
+        List<Task> tasksList = new ArrayList<>();
+        tasksList.remove(id);
+    }*/
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public Optional<Task> getTask(final Long id) {
+        return repository.findById(id);
     }
 }
