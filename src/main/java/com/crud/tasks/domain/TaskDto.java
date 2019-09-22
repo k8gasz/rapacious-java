793f545abc4e0a59a3a114ac278638540c620858
k8gasz <gaszewska@pm.me>
2019-09-22 19:41:35 +0200

    CREATE, READ, UPDATE and DELETE methods implementation.

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -4,20 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class TaskDto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-
     private String title;
     private String content;
 }
